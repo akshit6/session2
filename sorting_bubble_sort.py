a6f866acc847ccf17e65688b3089fbdc40d1645c
akshit6 <akshit.gs@somaiya.edu>
2019-06-13 20:05:09 +0530

    very first commit of bubble sort

--- sorting_bubble_sort.py
+++ sorting_bubble_sort.py
@@ -1,1 +1,11 @@
 
+a = list(map(int,input().split()))
+len = len(a)
+
+for i in range(len-1):
+    for j in range(len-i-1):
+        if a[j] > a[j+1]:
+            a[j],a[j+1] = a[j+1],a[j]
+print("\nThe sorted array is:")
+print(a)
+
