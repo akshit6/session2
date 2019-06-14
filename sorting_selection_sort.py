1c7ab714af62965315cb27742ad62d294b464924
akshit6 <akshit.gs@somaiya.edu>
2019-06-14 19:21:09 +0530

    very first commit of selection sort

--- sorting_selection_sort.py
+++ sorting_selection_sort.py
@@ -1,1 +1,17 @@
+print("Enter the unsorted array:")
+a = list(map(int, input().split()))
+len = len(a)
+print(a)
+min = a[0]
 
+for i in range(len - 1):
+    min = a[i]
+    index = i
+    for j in range(i + 1, len):
+        if a[j] < min:
+            index = j
+            min = a[j]
+
+    a[i], a[index] = a[index], a[i]
+print("The sorted array is:")
+print(a)
